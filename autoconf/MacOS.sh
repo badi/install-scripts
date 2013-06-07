d20a487a479e73627bddef3ebac8e3bf2c1cc884
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-06-07 10:57:15 -0400

    new package: autoconf

---
+++ autoconf/MacOS.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+globals=$( cd $(dirname $0); pwd)/../globals.sh
+source $globals
+
+NAME=autoconf
+VERSION=2.69
+URL=http://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.tar.xz
+
+curl -L $URL | unxz - | tar xf -
+cd $NAME-$VERSION
+./configure --prefix=$PREFIX/$NAME/$VERSION --enable-static --enable-shared 
+make $MAKEOPTS
+$SUDO make install
+
+modulefile=$MODULEFILES/$NAME/$VERSION
+mkdir -p $(dirname $modulefile)
+cat <<EOF>$modulefile
+#%Module
+
+set name $NAME
+set ver  $VERSION
+set pref $PREFIX
+
+set root \$pref/\$name/\$ver
+
+prepend-path PATH     \$root/bin
+prepend-path INFOPATH \$root/share/info
+prepend-path MANPATH  \$root/share/man
+EOF
